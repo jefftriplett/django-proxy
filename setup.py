1f3db5c64653791a9d53419488c4d036a449bcec
Jeff Triplett <jeff.triplett@gmail.com>
2013-11-06 17:43:23 -0600

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-proxy',
-    version='0.2.1',
+    version='0.2.2',
     description='This is a content aggregation solution via proxy intermediary models.',
     author='Kevin Fricovsky',
     author_email='kevin@montylounge.com',
